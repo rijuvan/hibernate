cef1de8285f8649f18575a4f8c0f04748c4cbd71
Rijuvan.ansari <rijuvanansari@yahoo.com>
2018-03-15 21:41:04 +0530

    BCt Day 2

---
+++ inheritanceTablePerClass/src/EmpHibernatetTest.java
@@ -0,0 +1,39 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+import com.soft.model.ContractEmployee;
+import com.soft.model.Employee;
+import com.soft.model.RegularEmployee;
+
+
+public class EmpHibernatetTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee employee=new Employee();
+		employee.setName("Amit Singh");
+		ContractEmployee cemployee=new ContractEmployee();
+		cemployee.setName("Contract EMP Anuj");
+		cemployee.setContractperiod("two year");
+		cemployee.setPayperhour(128);
+		RegularEmployee remployee=new RegularEmployee();
+		remployee.setName("Regular EMP Pawan");
+		remployee.setBonus(3000);
+		remployee.setSallary(12345);
+		
+		SessionFactory sessionFactory=	new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session=sessionFactory.openSession();
+		
+		session.beginTransaction();
+		session.save(employee);
+		session.save(cemployee);
+		session.save(remployee);
+		session.beginTransaction().commit();
+
+	}
+
+}
---
+++ inheritanceTablePerClass/src/com/soft/model/RegularEmployee.java
@@ -0,0 +1,35 @@
+package com.soft.model;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="RegularEmployee11")
+
+public class RegularEmployee  extends Employee{
+private float sallary;
+public float getSallary() {
+	return sallary;
+}
+public void setSallary(float sallary) {
+	this.sallary = sallary;
+}
+public int getBonus() {
+	return bonus;
+}
+public void setBonus(int bonus) {
+	this.bonus = bonus;
+}
+public RegularEmployee() {
+	
+	// TODO Auto-generated constructor stub
+}
+public RegularEmployee(float sallary, int bonus) {
+	
+	this.sallary = sallary;
+	this.bonus = bonus;
+}
+private int bonus;
+}
